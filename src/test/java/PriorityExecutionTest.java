2014992ae8d930d2e52719ab9869ad1e4053ac01
Sebastian Lozano <jlozanop42@gmail.com>
2023-11-28 10:17:03 -0500

    Added priority execution

--- src/test/java/PriorityExecutionTest.java
+++ src/test/java/PriorityExecutionTest.java
@@ -2,21 +2,23 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-@Test
 public class PriorityExecutionTest {
     @BeforeClass
     public void setup() {
         System.out.println("Set up chrome system variable");
     }
 
+    @Test(priority = 1)
     public void signIn() {
         System.out.println("Sign in");
     }
 
+    @Test(priority = 2)
     public void searchTShirt() {
         System.out.println("Search T-Shirt");
     }
 
+    @Test(priority = 3)
     public void signOut() {
         System.out.println("Sign out");
     }
