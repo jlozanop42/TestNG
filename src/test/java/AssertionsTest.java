564394851e6e48e84db23fd22944f7b84cb828a6
Sebastian Lozano <jlozanop42@gmail.com>
2023-11-30 09:36:53 -0500

    Assertions page added

---
+++ src/test/java/AssertionsTest.java
@@ -0,0 +1,30 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AssertionsTest {
+    @BeforeClass
+    public void setup() {
+        System.out.println("Set up chrome system variable");
+    }
+
+    @Test(priority = 1)
+    public void signIn() {
+        System.out.println("Sign in");
+    }
+
+    @Test(priority = 2)
+    public void searchTShirt() {
+        System.out.println("Search T-Shirt");
+    }
+
+    @Test(priority = 3)
+    public void signOut() {
+        System.out.println("Sign out");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        System.out.println("Clean up chrome cookies");
+    }
+}
