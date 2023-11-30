7e102f88503f736b0baecfcfe5030dfab231fe90
Sebastian Lozano <jlozanop42@gmail.com>
2023-11-30 09:55:33 -0500

    Soft assertions completed

--- src/test/java/HardAssertionsTest.java
+++ src/test/java/HardAssertionsTest.java
@@ -2,8 +2,12 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class HardAssertionsTest {
+
+    SoftAssert softAssert = new SoftAssert();
+    //Soft assert will continue the execution of the test, It will not skip to the next annotation
     @BeforeClass
     public void setup() {
         System.out.println("Set up chrome system variable");
@@ -11,13 +15,17 @@ public class HardAssertionsTest {
 
     @Test
     public void assertionsTest() {
+        // With soft assert we need to use assertAll at the end of the code
+        // otherwise it will pass the test even if there is a failure
         System.out.println("Sign in");
-        Assert.assertEquals(true, true, "The welcome link is not as expected");
+        softAssert.assertEquals(true, true, "The welcome link is not as expected");
         System.out.println("Assert equals completed");
-        Assert.assertTrue(false, "Dashboard is not displayed");
+        softAssert.assertTrue(false, "Dashboard is not displayed");
         System.out.println("Assert true completed");
-        Assert.assertFalse(true, "Welcome button is not displayed");
+        softAssert.assertFalse(true, "Welcome button is not displayed");
         System.out.println("Assert false completed");
+
+        softAssert.assertAll();
     }
 
     @AfterClass
