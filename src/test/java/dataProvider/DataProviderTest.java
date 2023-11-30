4a3b5876bb09d0e41524f8b73333186b9e8e72ea
Sebastian Lozano <jlozanop42@gmail.com>
2023-11-30 11:59:59 -0500

    Data provider method added to perfom data driven testing

---
+++ src/test/java/dataProvider/DataProviderTest.java
@@ -0,0 +1,23 @@
+package dataProvider;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderTest {
+
+    @Test(dataProvider = "loginData")
+    public void login(String email, String password, boolean passed) {
+        System.out.println("Login credentials: " + "\n" +
+                "email: " + email + "\n" +
+                "password: " + password + "\n" +
+                "Sucessfull login: " + passed);
+
+    }
+
+    @DataProvider
+    public Object[][] loginData() {
+        return new Object[][]{{"myemail@gmail.com", "1234pwd", true},
+                {"mySecondEmail@gmail.com", "1234qwerty", false},
+                {"myThird@gmail.com", "1234asdf", true}};
+    }
+}
