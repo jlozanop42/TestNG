93b910e64548d997d1d7bbba80a35faba6069bbd
Sebastian Lozano <jlozanop42@gmail.com>
2023-11-30 11:23:25 -0500

    Added dependsOnGroups attribute in test method

--- src/test/java/DependencyTest.java
+++ src/test/java/DependencyTest.java
@@ -3,37 +3,37 @@ import org.testng.annotations.Test;
 public class DependencyTest {
     // OrangeHRM demo https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
 
-    @Test
+    @Test(groups = "initialize")
     public void test1SetupChrome() {
         System.out.println("Test 1: Setup chrome");
     }
 
-    @Test(dependsOnMethods = "test1SetupChrome")
+    @Test(dependsOnGroups = "initialize", groups = "env_application")
     public void test2OpenOrangeHRM() {
         System.out.println("Test 2: Open Application");
     }
 
-    @Test(dependsOnMethods = "test2OpenOrangeHRM")
+    @Test(dependsOnGroups = "env_application")
     public void test3SignIn() {
         System.out.println("Test 3: Sign in");
     }
 
-    @Test(dependsOnMethods = {"test2OpenOrangeHRM", "test3SignIn"})
+    @Test(dependsOnGroups = "env_application")
     public void test4SearchUser() {
         System.out.println("Test 4: Search for a user");
     }
 
-    @Test(dependsOnMethods = {"test2OpenOrangeHRM", "test3SignIn"})
+    @Test(dependsOnGroups = "env_application")
     public void test5SearchEmployee() {
         System.out.println("Test 5: Search employee");
     }
 
-    @Test(dependsOnMethods = {"test2OpenOrangeHRM", "test3SignIn"})
+    @Test(dependsOnGroups = "env_application")
     public void test6SearchCandidate() {
         System.out.println("Test 6: Search candidate");
     }
 
-    @Test(dependsOnMethods = "test3SignIn")
+    @Test(dependsOnGroups = "env_application")
     public void test7SignOut() {
         System.out.println("Test 7: Sign out");
     }
